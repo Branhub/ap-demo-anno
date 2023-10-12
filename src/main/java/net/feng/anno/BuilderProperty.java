91905a6ddba5985f3b03db0aecfecb79321941a9
Branhub <fkqah64@126.com>
2023-10-13 01:24:32 +0800

    temp

---
+++ src/main/java/net/feng/anno/BuilderProperty.java
@@ -0,0 +1,13 @@
+package net.feng.anno;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface BuilderProperty
+{
+    Class<?> classEle() default Integer.class;
+}
